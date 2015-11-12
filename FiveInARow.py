1f783a990b3921ae78010fea9a9cbdc7bffc4c01
U-ET\zspicklemire <zspicklemire@IN7543.ET.LOCAL>
2015-11-12 10:18:20 -0500

    Update Master

--- FiveInARow.py
+++ FiveInARow.py
@@ -1,5 +1,6 @@
 import http.client as httpc
 import http.server as https
+import random
 import sys
 
 def main(argv):
@@ -9,18 +10,24 @@ def main(argv):
     return
 
 class MyHandler(https.BaseHTTPHandler):
+
+    last_path = ''
+
+    opponent = ''
+
+    def do_GET(self):
+        port = self.path[1:]
+        MyHandler.opponent = self.client_address[0] + ':' + port
+        return
     
     def do_POST(self):
+        MyHandler.last_path = self.path
         return
 
 
 class Game():
 
     def __init__(self):
-        port = int(input("enter a port number to run on"))
-        # set up server
-        self.server = https.BaseHTTPServer(('',port),MyHander)
-        # handshake
         self.boardsize = 19
         self.board = [['.' for x in range(self.boardsize)] for y in range(self.boardsize)]
         self.own_moves = []
@@ -28,16 +35,56 @@ class Game():
     
     def run(self):
         """ them main loop for the game """
-        # Pick first player
-        # Loop
-        #  Wait for opponent to move
-        #  Check if game is won
-        #  Prompt for move
-        #  Send move to opponent
-        # Cleanup
+        port = int(input("enter a port number to run on"))
+        # set up server
+        self.server = https.BaseHTTPServer(('',port),MyHander)
+        print("one player must listen first, the other player will must reach out")
+        is_listener = int(input("enter 1 if to be the listener, 0 to if the other player will be: "))
+        if (is_listener == 1):
+            self.server.handler_request()
+            if random.randint(0,1) == 1:
+                self.print_board()
+                self.send_move()
+            else:
+                self.send_you_go()
+        else:
+            self.handshake(port)
+        while True:
+            self.server.handle_request()
+            if MyHandler.last_path == "/youwin":
+                print("you win!")
+                break
+            self.print_board()
+            if MyHandler.last_path[:5] == "/move":
+                self.process_move(MyHandler.last_path[-4:])
+            move = input("input move: ")
+            self.send_move()
+        return
+
+    def process_move(self, move):
+        x, y = move[:2], move[2:]
+        opp_moves.append((int(x),int(y)))
+        return
+
+    def send_move(self):
+        move = input("enter move: ")
+        x,y = move.split(',')
+        move_string = x.strip().zfill(2) + y.strip().zfill(2)
+        httpc.HTTPConnection(MyHandler.opponent)
+        httpc.requset("POST", "/move/" + move_string)
+        return
+
+    def self_handshake(self, my_port):
+        host = input("enter the other players address ")
+        port = input("enter the other players port: ")
+        MyHandler.opponent = host + ':' + port
+        httpc.HTTPConnection(MyHandler.opponent[0], MyHandler.opponent[1])
+        httpc.request("GET", '/'+str(my_port))
         return
 
     def print_board(self):
+        for i in range(100):
+            print("")
         row_string = '   '
         for i in range(boardsize):
             rowstring += str(i).rjust(3)
